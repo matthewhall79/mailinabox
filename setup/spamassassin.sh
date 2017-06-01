e8a18e4aa0148edf850f94d462de23d8fc05f123
Matthew Hall <github@matthall.co.uk>
2017-06-01 21:50:38 +0000

    Fix bad syntax in SA setup script.

--- setup/spamassassin.sh
+++ setup/spamassassin.sh
@@ -140,7 +140,7 @@ EOF
 tools/editconf.py /etc/spamassassin/local.cf -s \
 	bayes_path="$STORAGE_ROOT/mail/spamassassin/bayes" \
 	bayes_file_mode=0666 \
-	score BAYES_999  0  0  1.6  1.6
+	score="BAYES_999  0  0  1.6  1.6"
 
 mkdir -p "$STORAGE_ROOT/mail/spamassassin"
 chown -R spampd:spampd "$STORAGE_ROOT/mail/spamassassin"
